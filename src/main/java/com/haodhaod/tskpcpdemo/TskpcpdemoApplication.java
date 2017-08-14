9090eb4b77d0a59586f22fdbf11806cef0e627dc
tskpcp <tskpcp@126.com>
2017-08-14 11:43:44 +0800

    add2017-8-14

---
+++ src/main/java/com/haodhaod/tskpcpdemo/TskpcpdemoApplication.java
@@ -0,0 +1,12 @@
+package com.haodhaod.tskpcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TskpcpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TskpcpdemoApplication.class, args);
+	}
+}
